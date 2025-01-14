d51cea9df668dcfeb3bfddd307566b33982784bf
MikhailSorokin <sorokinmike96@gmail.com>
2025-01-14 12:20:09 -0500

    Fix some links week 4.

--- assigned_problems/Week4/23-MergeSortedLists.py
+++ assigned_problems/Week4/23-MergeSortedLists.py
@@ -5,5 +5,6 @@
 #         self.next = next
 class Solution:
     # Apple - Saman
+    # https://leetcode.com/problems/merge-k-sorted-lists/description/
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
--- assigned_problems/Week4/31-NextPermutation.py
+++ assigned_problems/Week4/31-NextPermutation.py
@@ -1,7 +1,5 @@
 class Solution:
     # Mike - Meta
-
-    #https://leetcode.com/problems/element-appearing-more-than-25-in-sorted-array/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
     #https://leetcode.com/problems/next-permutation/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
     def nextPermutation(self, nums: List[int]) -> None:
         """

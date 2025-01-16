20703f99a6b498265ad9d6b9c884f59b94659040
MikhailSorokin <sorokinmike96@gmail.com>
2025-01-16 18:36:59 -0500

    Correcting solution to never store a 64-bit number.

--- assigned_problems/Week5/7 - ReverseInteger (Meta).py
+++ assigned_problems/Week5/7 - ReverseInteger (Meta).py
@@ -2,23 +2,18 @@ class Solution:
     # Mike 
     # https://leetcode.com/problems/reverse-integer/
     def reverse(self, x: int) -> int:
-            queue = []
-            mul = -1 if x < 0 else 1
-            x = -x if x < 0 else x
+        mul = -1 if x < 0 else 1
+        x = -x if x < 0 else x
 
-            while x != 0:
-                queue.append(x % 10)
-                x = x // 10
+        res = 0
+        while x != 0:
+            modNum = x % 10
+            x = x // 10
 
-            res = 0
-            while queue:
-                n = len(queue)
-                front = queue.pop(0)
-                res += pow(10, n - 1) * front
-
-            # NOTE: This problem requires us to have <= 2^31 - 1
-            if res > pow(2, 31) - 1:
+            if res > (pow(2, 31) - 1) // 10:
                 return 0
 
-            return res * mul
+            res = (res * 10) + modNum
+
+        return res * mul
         

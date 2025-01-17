f5fb1884ee140d1a19ae05bccbf1ba6ad21a4cdc
MikhailSorokin <sorokinmike96@gmail.com>
2025-01-17 07:34:53 -0500

    Week 6 for me and Rodrigo.

---
+++ assigned_problems/Week6/3101-CountAlternatingSubarrays-CapitalOne.py
@@ -0,0 +1,5 @@
+class Solution:
+    # Rodrigo - Capital One
+    # https://leetcode.com/problems/count-alternating-subarrays/?envType=company&envId=capital-one&favoriteSlug=capital-one-six-months
+    def countAlternatingSubarrays(self, nums: List[int]) -> int:
+        
---
+++ assigned_problems/Week6/38-CountAndSay-Meta.py
@@ -0,0 +1,5 @@
+class Solution:
+    # Mike - Meta
+    # https://leetcode.com/problems/count-and-say/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+    def countAndSay(self, n: int) -> str:
+        

37e138fe71c44503839ebbafa54562d23527bcd5
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2025-01-17 19:42:11 -0500

    Update 71 - SimpifyPath (Google).py

--- assigned_problems/Week5/71 - SimpifyPath (Google).py
+++ assigned_problems/Week5/71 - SimpifyPath (Google).py
@@ -2,4 +2,17 @@ class Solution:
     # Daniel - Google 
     # https://leetcode.com/problems/simplify-path/description/
     def simplifyPath(self, path: str) -> str:
-        
+        stack = []
+        cur = ""
+
+        for c in path + "/":
+            if c == "/":
+                if cur == "..":
+                    if stack: stack.pop()
+                elif cur != "" and cur != ".":
+                    stack.append(cur)
+                cur = ""
+            else:
+                cur += c
+
+        return "/" + "/".join(stack)

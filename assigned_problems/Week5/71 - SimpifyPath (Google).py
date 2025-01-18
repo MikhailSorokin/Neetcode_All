42a17307d44949b41937312d73d9a6859cea3648
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2025-01-17 19:46:32 -0500

    commented SimpifyPath (Google).py

--- assigned_problems/Week5/71 - SimpifyPath (Google).py
+++ assigned_problems/Week5/71 - SimpifyPath (Google).py
@@ -5,14 +5,14 @@ class Solution:
         stack = []
         cur = ""
 
-        for c in path + "/":
-            if c == "/":
-                if cur == "..":
-                    if stack: stack.pop()
+        for c in path + "/": # iterate through each char in path
+            if c == "/": # evaluate if we hit a slash 
+                if cur == "..": 
+                    if stack: stack.pop() # go up one level
                 elif cur != "" and cur != ".":
-                    stack.append(cur)
-                cur = ""
-            else:
-                cur += c
+                    stack.append(cur) # record current folder or file
+                cur = "" # will eat up repetitive slashes and "."
+            else: 
+                cur += c # build the path
 
-        return "/" + "/".join(stack)
+        return "/" + "/".join(stack) # we want the path to start with "/"

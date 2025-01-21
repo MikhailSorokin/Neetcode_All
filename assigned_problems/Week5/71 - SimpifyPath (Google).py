394391ca9d956ee380b9b152e04a24bbe1bccd72
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2025-01-21 06:42:34 -0500

    add example 71 - SimpifyPath (Google).py

--- assigned_problems/Week5/71 - SimpifyPath (Google).py
+++ assigned_problems/Week5/71 - SimpifyPath (Google).py
@@ -3,16 +3,44 @@ class Solution:
     # https://leetcode.com/problems/simplify-path/description/
     def simplifyPath(self, path: str) -> str:
         stack = []
-        cur = ""
+        curr_dir = ""
 
         for c in path + "/": # iterate through each char in path
             if c == "/": # evaluate if we hit a slash 
-                if cur == "..": 
-                    if stack: stack.pop() # go up one level
-                elif cur != "" and cur != ".":
-                    stack.append(cur) # record current folder or file
-                cur = "" # will eat up repetitive slashes and "."
+                if curr_dir == "..": 
+                    if stack: stack.pop() # go up one level to parent directory
+                elif curr_dir != "" and curr_dir != ".": # if current directory non-trivial
+                    stack.append(curr_dir) # record current folder or file
+                curr_dir = "" # will eat up repetitive slashes and single periods in curr_dir
             else: 
-                cur += c # build the path
+                curr_dir += c # build the path
 
         return "/" + "/".join(stack) # we want the path to start with "/"
+
+        # # example 5
+        # stack = []
+        # curr_dir = "..."
+
+        # stack = ["..."]
+        # curr_dir = "a"
+
+        # stack = ["...", "a"]
+        # curr_dir = ".."
+
+        # stack = ["..."]
+        # curr_dir = "b"
+
+        # stack = ["...", "b"]
+        # curr_dir = "c"
+
+        # stack = ["...", "b", "c"]
+        # curr_dir = ".."
+
+        # stack = ["...", "b"]
+        # curr_dir = "d"
+
+        # stack = ["...", "b", "d"]
+        # curr_dir = "."
+
+        # # join
+        # "/.../b/d/"

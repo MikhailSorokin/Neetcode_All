760ddac4786ed9e4c4b1517e22570c760c3ee336
MikhailSorokin <sorokinmike96@gmail.com>
2025-01-21 05:59:29 -0500

    Binary search solution for capacity to ship packages.

--- assigned_problems/Week7/1011-CapacityToShipPackages-Meta.py
+++ assigned_problems/Week7/1011-CapacityToShipPackages-Meta.py
@@ -1,4 +1,32 @@
 class Solution:
     # Meta - https://leetcode.com/problems/capacity-to-ship-packages-within-d-days/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+    def isFeasible(self, weights, capacity, days):
+        daysNeeded = 1
+        currentLoad = 0
+        for weight in weights:
+            currentLoad += weight
+            if currentLoad > capacity:
+                daysNeeded += 1
+                currentLoad = weight
+
+        return daysNeeded <= days
+
     def shipWithinDays(self, weights: List[int], days: int) -> int:
+        totalLoad = 0
+        maxLoad = 0
+        for weight in weights:
+            totalLoad += weight
+            maxLoad = max(maxLoad, weight)
+
+        l = maxLoad
+        r = totalLoad
+
+        while l < r:
+            mid = (l + r) // 2
+            if self.isFeasible(weights, mid, days):
+                r = mid
+            else:
+                l = mid + 1
+
+        return l
         

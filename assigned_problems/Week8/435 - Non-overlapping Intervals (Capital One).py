7049f404c7a70e6153826bec5f7cc08095089384
MikhailSorokin <sorokinmike96@gmail.com>
2025-01-23 16:13:08 -0500

    Non-overlapping intervals - Rodrigo and String Compression - Saman.

---
+++ assigned_problems/Week8/435 - Non-overlapping Intervals (Capital One).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Rodrigo
+    # https://leetcode.com/problems/non-overlapping-intervals/?envType=company&envId=capital-one&favoriteSlug=capital-one-six-months
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
---
+++ assigned_problems/Week8/443 - String Compression (Apple).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Saman 
+    # https://leetcode.com/problems/string-compression/?envType=company&envId=apple&favoriteSlug=apple-three-months
+    def compress(self, chars: List[str]) -> int:
+        

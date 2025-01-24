7e2cdf9bda03e8e69e5de3dbba2ff8b557fc4a8a
MikhailSorokin <sorokinmike96@gmail.com>
2025-01-23 23:42:30 -0500

    Search in rotated array.

--- assigned_problems/Week8/33 - Search in Rotated Array (Meta).py
+++ assigned_problems/Week8/33 - Search in Rotated Array (Meta).py
@@ -1,4 +1,28 @@
 class Solution:
     # Mike - Meta #https://leetcode.com/problems/search-in-rotated-sorted-array/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
     def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+
+        while left < right:
+            # Prevents potential memory overflow
+            mid = left + (right - left) // 2
+            # Check which one has a valid range and if we fall in it
+            if nums[mid] == target:
+                return mid
+            elif nums[left] <= nums[mid]:
+                # Now, check if we fall in that range
+                if nums[left] <= target <= nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] <= target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        if nums[left] == target:
+            return left
+        return -1
         

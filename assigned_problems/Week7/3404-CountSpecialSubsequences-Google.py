3f7f9e8cf91c47d72d4c71a21d007501c3a55169
saitamasan0220 <103046751+saitamasan0220@users.noreply.github.com>
2025-01-25 08:41:30 -0500

    Update 3404-CountSpecialSubsequences-Google.py

--- assigned_problems/Week7/3404-CountSpecialSubsequences-Google.py
+++ assigned_problems/Week7/3404-CountSpecialSubsequences-Google.py
@@ -1,4 +1,13 @@
 class Solution:
     # Daniel - https://leetcode.com/problems/count-special-subsequences/?envType=company&envId=google&favoriteSlug=google-thirty-days
-    def numberOfSubsequences(self, nums: List[int]) -> int:
-        
+    def numberOfSubsequences(self, A: List[int]) -> int:
+        n = len(A)
+        cnt = Counter()
+        res = 0
+        for r in range(4, n - 2):
+            q = r - 2
+            for p in range(q - 1):
+                cnt[A[p] / A[q]] += 1
+            for s in range(r + 2, n):
+                res += cnt[A[s] / A[r]]
+        return res                

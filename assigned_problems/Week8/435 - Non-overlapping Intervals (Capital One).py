537de852f3899e3c34f5ed6bdbb17694a14735bf
Rodrigo Pimenta <11894505+rodrigopimenta10@users.noreply.github.com>
2025-02-01 12:41:55 -0500

    Update 435 - Non-overlapping Intervals (Capital One).py

--- assigned_problems/Week8/435 - Non-overlapping Intervals (Capital One).py
+++ assigned_problems/Week8/435 - Non-overlapping Intervals (Capital One).py
@@ -2,4 +2,28 @@ class Solution:
     # Rodrigo
     # https://leetcode.com/problems/non-overlapping-intervals/?envType=company&envId=capital-one&favoriteSlug=capital-one-six-months
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
-        
+        if not intervals:
+            return 0
+
+        intervals.sort(key=lambda x:x[1])
+
+        prev_end = float('-inf')
+        #non_overlapping_count = 0
+        overlapping_count = 0
+        #res = [intervals[0]]
+        ptr = 0
+        #n = len(intervals)
+        while ptr < len(intervals):
+            start, end = intervals[ptr]
+            interval = intervals[ptr]
+            if start >= prev_end:
+                prev_end = end
+                #res.append(intervals[i])
+                ptr += 1
+            else:
+                overlapping_count += 1
+                intervals.remove(interval)
+                #Don't increment pointer
+
+        #print(f'{intervals}')
+        return overlapping_count

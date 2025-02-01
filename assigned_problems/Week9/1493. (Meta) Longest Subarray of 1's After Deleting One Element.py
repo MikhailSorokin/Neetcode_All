e389452493d51d4cdd853f42f4f1a0631cbff246
MikhailSorokin <sorokinmike96@gmail.com>
2025-02-01 09:59:50 -0500

    Upload of week 9 solution.

--- assigned_problems/Week9/1493. (Meta) Longest Subarray of 1's After Deleting One Element.py
+++ assigned_problems/Week9/1493. (Meta) Longest Subarray of 1's After Deleting One Element.py
@@ -1,3 +1,19 @@
 class Solution
     # Mike - https://leetcode.com/problems/longest-subarray-of-1s-after-deleting-one-element/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
     def longestSubarray(self, nums: List[int]) -> int:
+        zeroCount = 0
+        longestWindow = 0
+        start = 0
+        n = len(nums)
+
+        for i in range(n):
+            zeroCount += (nums[i] == 0)
+
+            # Shrink until count of zeros is <= 1
+            while zeroCount > 1:
+                zeroCount -= (nums[start] == 0)
+                start += 1
+
+            longestWindow = max(longestWindow, i - start)
+
+        return longestWindow

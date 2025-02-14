a7ac27ae3cbef6acdedf3323f80356384c474bb8
MikhailSorokin <sorokinmike96@gmail.com>
2025-02-14 10:51:34 -0500

    Sliding window median problem.

---
+++ assigned_problems/Week11/480 - Sliding Window Median.py
@@ -0,0 +1,25 @@
+class Solution:
+    # Mike
+    # Part One
+    # https://leetcode.com/problems/find-median-from-data-stream/description/
+    class MedianFinder:
+
+        def __init__(self):
+            
+
+        def addNum(self, num: int) -> None:
+            
+
+        def findMedian(self) -> float:
+            
+
+
+    # Your MedianFinder object will be instantiated and called as such:
+    # obj = MedianFinder()
+    # obj.addNum(num)
+    # param_2 = obj.findMedian()
+    
+    # Part Two
+    # https://leetcode.com/problems/sliding-window-median/description/?envType=company&envId=facebook&favoriteSlug=facebook-thirty-days
+    def medianSlidingWindow(self, nums: List[int], k: int) -> List[float]:
+        

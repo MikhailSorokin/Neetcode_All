e6c5f48417003a109eb869ab0a36abb6ce41bd3c
Rodrigo Pimenta <11894505+rodrigopimenta10@users.noreply.github.com>
2025-02-17 21:03:41 -0500

    Update 2251 - Number Flowers Full Bloom (Capital One).py

--- assigned_problems/Week11/2251 - Number Flowers Full Bloom (Capital One).py
+++ assigned_problems/Week11/2251 - Number Flowers Full Bloom (Capital One).py
@@ -2,4 +2,15 @@ class Solution:
     # Rodrigo
     # https://leetcode.com/problems/number-of-flowers-in-full-bloom/?envType=company&envId=capital-one&favoriteSlug=capital-one-all
     def fullBloomFlowers(self, flowers: List[List[int]], people: List[int]) -> List[int]:
-        
+        starts = sorted([start for start, _ in flowers])
+        ends = sorted([end for _, end in flowers])
+
+        res = []
+        for t in people:
+
+            started = bisect_right(starts,t)
+
+            ended = bisect_left(ends, t)
+
+            res.append(started - ended)
+        return res

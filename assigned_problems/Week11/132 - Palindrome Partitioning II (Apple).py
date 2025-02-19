de696ac8571b43b766d9ecb8e26588a2842e6f0b
MikhailSorokin <sorokinmike96@gmail.com>
2025-02-19 00:07:12 -0500

    Week 11.

---
+++ assigned_problems/Week11/132 - Palindrome Partitioning II (Apple).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Saman
+    # https://leetcode.com/problems/palindrome-partitioning-ii/?envType=company&envId=apple&favoriteSlug=apple-all
+    def minCut(self, s: str) -> int:
+        
---
+++ assigned_problems/Week11/1994 - The Number Of Good Subsets (Google).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Google
+    # https://leetcode.com/problems/the-number-of-good-subsets/?envType=company&envId=google&favoriteSlug=google-all
+    def numberOfGoodSubsets(self, nums: List[int]) -> int:
+        

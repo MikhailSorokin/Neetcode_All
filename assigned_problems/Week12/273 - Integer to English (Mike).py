53cf5ae691ef1fa8d7246d4d526d748fb968f316
MikhailSorokin <sorokinmike96@gmail.com>
2025-02-22 13:42:58 -0500

    Week 12 problems assigned for me and Rodrigo.

---
+++ assigned_problems/Week12/273 - Integer to English (Mike).py
@@ -0,0 +1,4 @@
+class Solution:
+    # Roblox
+    # https://leetcode.com/problems/integer-to-english-words/?envType=company&envId=roblox&favoriteSlug=roblox-all
+    def numberToWords(self, num: int) -> str:
---
+++ assigned_problems/Week12/546 - Remove Boxes (Rodrigo).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Capital One
+    # https://leetcode.com/problems/remove-boxes/?envType=company&envId=capital-one&favoriteSlug=capital-one-all
+    def removeBoxes(self, boxes: List[int]) -> int:
+        

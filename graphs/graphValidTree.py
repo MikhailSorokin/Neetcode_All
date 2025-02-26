d72e91de4aa539b1b74970b15658ea058a401324
MikhailSorokin <sorokinmike96@gmail.com>
2025-02-26 13:00:29 -0500

    Valid tree, valid sudoku.

--- graphs/graphValidTree.py
+++ graphs/graphValidTree.py
@@ -1,25 +1,64 @@
+from collections import deque, defaultdict
+
+# BFS Approach
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if len(edges) > n - 1:
+        if len(edges) != n - 1:
+            return False
+
+        graph = defaultdict(list)
+
+        for x, y in edges:
+            graph[x].append(y)
+            graph[y].append(x)
+
+        for i in range(0, n):
+            queue = deque([(i, None)])
+            visited = set()
+
+            while queue:
+                node, parent = queue.popleft()
+                for child in graph[node]:
+                    if child != parent:
+                        if child in visited:
+                            return False
+                        visited.add(child)
+                        queue.append((child, node))
+        
+        return True
+        
+# DFS Approach
+
+from collections import deque, defaultdict
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n-1:
             return False
         
-        adj = [[] for _ in range(n)]
-        for u, v in edges:
-            adj[u].append(v)
-            adj[v].append(u)
+        graph = defaultdict(list)
+
+        for x, y in edges:
+            #print(f'{x},{y},{graph}')
+            graph[x].append(y)
+            graph[y].append(x)
         
-        visit = set()
-        q = deque([(0, -1)])  # (current node, parent node)
-        visit.add(0)
+        visited = set()
+        def dfs(x, parent):
+            visited.add(x)
+            if graph[x]:
+                for child in graph[x]:
+                    if child not in visited:
+                        if dfs(child, x):
+                            return True
+                    elif child != parent:
+                        return True
+            return False
         
-        while q:
-            node, parent = q.popleft()
-            for nei in adj[node]:
-                if nei == parent:
-                    continue
-                if nei in visit:
+        for x in graph:
+            if x not in visited:
+                if dfs(x, None):
                     return False
-                visit.add(nei)
-                q.append((nei, node))
+        print(visited)
+        return True
         
-        return len(visit) == n

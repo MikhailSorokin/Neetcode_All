86f00c2b993e6bdcde9152426d4474a5ac198f4f
MikhailSorokin <sorokinmike96@gmail.com>
2025-03-02 20:38:57 -0500

    Adding problems for new week.

---
+++ assigned_problems/Week13/1606 - Find Servers Most Requests (Rodrigo).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Capital One
+    # https://leetcode.com/problems/find-servers-that-handled-most-number-of-requests/description/?envType=company&envId=capital-one&favoriteSlug=capital-one-all
+    def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
+        
---
+++ assigned_problems/Week13/220 - Contains Duplicate III (Mike).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Palantir
+    # https://leetcode.com/problems/contains-duplicate-iii/description/?envType=company&envId=palantir-technologies&favoriteSlug=palantir-technologies-all
+    def containsNearbyAlmostDuplicate(self, nums: List[int], indexDiff: int, valueDiff: int) -> bool:
+        

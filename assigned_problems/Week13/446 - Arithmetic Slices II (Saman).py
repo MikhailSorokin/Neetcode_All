4bf4c8cf4df7ec2990ca304347d8c6141edbf86a
MikhailSorokin <sorokinmike96@gmail.com>
2025-03-06 20:44:51 -0500

    Arithmetic slices II - Saman.

---
+++ assigned_problems/Week13/446 - Arithmetic Slices II (Saman).py
@@ -0,0 +1,5 @@
+class Solution:
+    # Apple
+    # https://leetcode.com/problems/arithmetic-slices-ii-subsequence/description/?envType=company&envId=apple&favoriteSlug=apple-all
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        

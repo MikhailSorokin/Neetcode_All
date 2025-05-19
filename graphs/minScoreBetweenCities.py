8ee2e5ceda93924b8b39f25c9af68ea6e91679a0
MikhailSorokin <sorokinmike96@gmail.com>
2025-05-19 06:50:16 -0400

    Min score between cities.

---
+++ graphs/minScoreBetweenCities.py
@@ -0,0 +1,29 @@
+from collections import deque, defaultdict
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        paths = deque()
+        minDist = float('inf')
+        adjGraph = defaultdict(list)
+
+        # Find where we can start at road 1
+        for road in roads:
+            roadA, roadB, cost = road 
+            adjGraph[roadA].append([roadB, cost])
+            adjGraph[roadB].append([roadA, cost])
+
+        vis = [False] * (n + 1)
+        paths.appendleft(1)
+        vis[1] = True
+
+        while paths:
+            node = paths.popleft()
+
+            # Add neighbors to queue UNLESS roadA is at n
+            for neighbor, cost in adjGraph[node]:
+                minDist = min(minDist, cost)
+                if vis[neighbor] == False:
+                    vis[neighbor] = True
+                    paths.appendleft(neighbor)
+        
+        return minDist

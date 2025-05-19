fc246a0798e985641420efa30ca6c599cf4e5710
MikhailSorokin <sorokinmike96@gmail.com>
2025-05-19 07:36:33 -0400

    Number substrings three characters.

---
+++ pointers/numberSubstringsThreeChars.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        n = len(s)
+        res = 0
+        counter = Counter()
+
+        left = 0
+        for right in range(n):
+            counter[s[right]] += 1
+            while counter['a'] > 0 and counter['b'] > 0 and counter['c'] > 0:
+                counter[s[left]] -= 1
+                left += 1
+                res += len(s) - right
+
+        return res
